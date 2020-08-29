bcdbd6100d4cebd8c7ca32582e827584d0b6f24f
Suhail <69284161+SuhailMontano@users.noreply.github.com>
2020-08-29 01:59:23 -0500

    Create Laboratorio 18.R

---
+++ Laboratorio 18.R
@@ -0,0 +1,50 @@
+Hecho con gusto por Suhail Montaño Sánchez (UAEH)
+#V6
+# LABORATORIO - Gráficos en R con ggplot2 para Ciencia de Datos
+# manipular leyendas
+
+
+
+#instalando paquete con los datos
+#install.packages("gapminder")
+#install.packages("ggplot2")
+
+
+
+# cargar libreria ggplot2 y gapminder
+library(ggplot2)
+library(gapminder)
+
+data("gapminder")
+gapminder2007 <- gapminder[gapminder$year == '2007', ]
+
+# colocando texto base
+p<-ggplot(data = gapminder2007, 
+          mapping = aes(x=gdpPercap,
+                        y=lifeExp,
+                        color=continent))+
+  geom_point()
+
+# mostrar la grafica
+
+p
+
+# quitar titulo de la leyenda
+
+p+theme(legend.title = element_blank())
+
+# quitar toda la leyenda
+
+p+theme(legend.position = "none")
+
+
+# cambiar la leyenda de posición
+# opciones: # rigth, left, bottom, top
+
+p+theme(legend.position = "top")
+
+# cambiar con coordenadas de 0 a 1
+
+p+theme(legend.position = c (0.5, 0.5))
+
+p+theme(legend.position = c (0.8, 0.3))
